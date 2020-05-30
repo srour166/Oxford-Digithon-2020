6b810cce5787d5a9bd73a9c924f49f528a50789b
Shubham Jain <shubham.jain@hotmail.com>
2020-05-30 19:17:44 +0100

    Remove mongo code
    The mongo module isn't working for some reason. No time to fix it.

--- routine/lib/main.dart
+++ routine/lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mongo_stitch/flutter_mongo_stitch.dart';
 import 'package:routine/choose_categories.dart';
 import 'package:routine/login.dart';
 import 'package:routine/register.dart';
 import 'package:routine/utils/constants.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  // await MongoStitchClient.initializeApp(MONGO_APP_ID);
-
+void main() {
   runApp(MyApp());
 }
 
