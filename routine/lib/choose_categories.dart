09af43ac1b3eb47f63fa19e3a5711deebab7e3d5
Youmna Srour <youmna.srour.19@ucl.ac.uk>
2020-05-30 22:07:51 +0100

    added categories

--- routine/lib/choose_categories.dart
+++ routine/lib/choose_categories.dart
@@ -1,59 +1,55 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:routine/utils/Category.dart';
 
 class ChooseCategories extends StatefulWidget {
+  const ChooseCategories();
   @override
   _ChooseCategoriesState createState() => _ChooseCategoriesState();
 }
 
 class _ChooseCategoriesState extends State<ChooseCategories> {
-  final List<Category> categories = [
-    Category(
-      description: 'test',
-      name: 'test',
-      imageUrl: 'https://via.placeholder.com/150',
-    ),
-    Category(
-      description: 'test 2',
-      name: 'test 2',
-      imageUrl: 'https://via.placeholder.com/150',
-    )
-  ];
-
+  List<int> selectedpics = [];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Choose your categories'),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Pick your interests'),
       ),
-      body: GridView.builder(
-        gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemCount: categories.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: Stack(
-              children: <Widget>[
-                Positioned.fill(
-                  child: Image.network(
-                    categories[index].imageUrl,
-                    fit: BoxFit.fill,
-                  ),
-                ),
-                Container(
-                    padding: EdgeInsets.all(10),
-                    alignment: Alignment.topCenter,
-                    child: Text(
-                      categories[index].name,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                      textScaleFactor: 1.2,
-                    )),
-              ],
-            ),
-          );
-        },
+      body: new GridView.extent(
+        maxCrossAxisExtent: 250.0,
+        mainAxisSpacing: 5.0,
+        crossAxisSpacing: 5.0,
+        padding: const EdgeInsets.all(5.0),
+        children: _buildGridTiles(8),
       ),
     );
+  }
+
+  List<Widget> _buildGridTiles(numberOfTiles) {
+    List<Container> containers =
+        new List<Container>.generate(numberOfTiles, (int index) {
+      final imageName = index < 9
+          ? 'images/image0${index + 1}.jpeg'
+          : 'images/image${index + 1}.jpeg';
+      return new Container(
+        child: GestureDetector(
+            onTap: () {
+              setState(() {
+                if (selectedpics.contains(index)) {
+                  selectedpics.remove(index);
+                } else {
+                  selectedpics.add(index);
+                }
+              });
+              print("pressing image");
+            },
+            child: selectedpics.contains(index)
+                ? ColorFiltered(
+                    colorFilter:
+                        ColorFilter.mode(Colors.red[100], BlendMode.modulate),
+                    child: Image.asset(imageName, fit: BoxFit.fill))
+                : Image.asset(imageName, fit: BoxFit.fill)),
+      );
+    });
+    return containers;
   }
 }
